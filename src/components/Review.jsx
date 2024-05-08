9f52c7f71ffc552e338c8db97a532725bd64b63f
+ <omuhindaaaron@gmail.com>
2024-05-08 10:09:35 +0300

    refactor: footer and review contents

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import img1 from '../assets/img/pic1.png'
-import img2 from '../assets/img/pic1.png'
-import img3 from '../assets/img/pic1.png'
+// import React from 'react'
+// import img1 from '../assets/img/pic1.png'
+// import img2 from '../assets/img/pic1.png'
+// import img3 from '../assets/img/pic1.png'
 
-const Review = () => {
-  return (
-    <div className=' min-h-screen flex flex-col justify-center lg:px-29 px-5 bg-backgroundColor'>
-        <h1 className=' font-semibold text-center text-4xl lg:mt-14 mt-24'>Please Leave Here Your Reviews</h1>
+// const Review = () => {
+//   return (
+//     <div className=' min-h-screen flex flex-col justify-center lg:px-29 px-5 bg-backgroundColor'>
+//         <h1 className=' font-semibold text-center text-4xl lg:mt-14 mt-24'>Please Leave Here Your Reviews</h1>
 
-        <div className=' flex flex-col lg:flex-row gap-5 justify-center py-4 my-8'>
-            //create a reviewcard to be used Here and import it above our review component
+//         <div className=' flex flex-col lg:flex-row gap-5 justify-center py-4 my-8'>
+//             //create a reviewcard to be used Here and import it above our review component
 
-            <ReviewCard  img={img1} title="Saint Cappuccino"/>
-            <ReviewCard  img={img2} title="King Coffee"/>
-            <ReviewCard  img={img3} title="Steve Styles"/>
-        </div>
-    </div>
-  )
-}
+//             <ReviewCard  img={img1} title="Saint Cappuccino"/>
+//             <ReviewCard  img={img2} title="King Coffee"/>
+//             <ReviewCard  img={img3} title="Steve Styles"/>
+//         </div>
+//     </div>
+//   )
+// }
 
-export default Review;
+// export default Review;
